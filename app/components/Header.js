5f6f26df79965f5882ca36bc6b427dfd37f90434
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-11-09 10:30:21 -0600

    nested post item so it can manage own comment state

--- app/components/Header.js
+++
@@ -1,9 +0,0 @@
-const React = require ('react');
-const PropTypes = require ('prop-types');
-
-//actual header
-
-
-
-
-module.exports = Header;
--- app/components/PostList.js
+++ app/components/PostList.js
@@ -26,50 +26,76 @@ function displayAge (age) {
 	}
 }     
 
+class PostItem extends React.Component {
+  render () {
+    return (
+      
+      <div 
+      className = 'postBox'>
+      <div className = 'postImg'>
+        <img id = 'postImg' src={this.props.post.img} alt={'img for '+ this.props.post.title}/> 
+      </div> 
+        <div className = 'vote'>
+          <img id = 'upvote' 
+              src="app/assets/thumb.png" 
+              alt="upvote"
+              onClick = {(e) => this.props.upvote(e,this.props.post.id)}
+              />
+          <img id = 'downvote' 
+              src="app/assets/thumb.png" 
+              alt="downvote"
+              onClick = {(e) => this.props.downvote(e,this.props.post.id)}
+              />
+          <h3 id= 'votecount'>{this.props.post.votes}</h3>
+
+        </div>
+      <div className= 'postTitle'>
+        <h2>{this.props.post.title}</h2>
+      </div>
+      <div className= 'postBody'>
+        <p>{this.props.post.body}</p>
+      <div className= 'author'>-{this.props.post.author}</div>
+      <p className='timestamp'>{this.props.post.comments} comment{this.props.post.comments !==1 ? 's' : ''} </p>
+      <p className= 'timestamp'>{displayAge(timeDifference(new Date(), this.props.post.timestamp))}</p>
+      </div>
+
+    
+    </div>
+
+    )
+  }
+}
+
+   /// PROPS that went to postList that now need to go to postItem
+{/*  PostList 
+        sortedPosts = {this.handleSort(this.props)}
+        upvote = {this.props.upvote}
+        downvote = {this.props.downvote}
+        filter = {this.props.filter}
+       */}
+
+
 class PostList extends React.Component {
   render() {
     return (
     this.props.sortedPosts.map( post => {
       return (
-       
-        <div 
-            key= {post.id}
-            className = 'postBox'>
-            <div className = 'postImg'>
-              <img id = 'postImg' src={post.img} alt={'img for '+ post.title}/> 
-            </div> 
-              <div className = 'vote'>
-                <img id = 'upvote' 
-                    src="app/assets/thumb.png" 
-                    alt="upvote"
-                    onClick = {(e) => this.props.upvote(e,post.id)}
-                    />
-                <img id = 'downvote' 
-                    src="app/assets/thumb.png" 
-                    alt="downvote"
-                    onClick = {(e) => this.props.downvote(e,post.id)}
-                    />
-                <h3 id= 'votecount'>{post.votes}</h3>
-
-              </div>
-            <div className= 'postTitle'>
-              <h2>{post.title}</h2>
-            </div>
-            <div className= 'postBody'>
-              <p>{post.body}</p>
-            <div className= 'author'>-{post.author}</div>
-            <p className='timestamp'>{post.comments} comment{post.comments !==1 ? 's' : ''} </p>
-            <p className= 'timestamp'>{displayAge(timeDifference(new Date(), post.timestamp))}</p>
-            </div>
-
-          
-          </div>
-
-
+       < PostItem
+          key = {post.id}
+          post = {post}
+          upvote = {this.props.upvote}
+          downvote = {this.props.downvote}
+           />
 
       )
     })
     )}
-}
+  }
+
+
+
+
 
 module.exports = PostList;
+
+  
